7c954484063f3f9742780f809b4683e74bcaa8be
customxx <fveramoran@gmail.com>
2017-02-14 12:46:33 -0300

    quinta

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def batman
   end
   def superman
-  	
+
   end
   def batman_vs_superman
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'pages/batman'
   get 'pages/superman'
   get 'pages/batman-vs-superman', to: "pages#batman_vs_superman"
-  root'pages#batman_vs_superman'
+  root 'pages#batman-vs-superman' 	
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
