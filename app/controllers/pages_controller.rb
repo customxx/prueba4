07a45a7063fd28f28dc018b18d6b1bb600191260
customxx <fveramoran@gmail.com>
2017-02-14 13:10:33 -0300

    sexta

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,13 @@ class PagesController < ApplicationController
   def batman_vs_superman
 
   end
+  def save_superman
+  	pages_save_superman.create(name: params[:name], email: params[:email])
+  	redirect_to pages_x_path, notice: "El usuario ha sido guardado"
+  end
+  def save_batmen
+  	pages_save_batmen.create(name: params[:name], email: params[:email])
+  	redirect_to pages_x_path, notice: "El usuario ha sido guardado"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  get 'experiments/page1'
+
+  get 'experiments/page2'
+
+  get 'experiments/page3'
+
   get 'pages/batman'
   get 'pages/superman'
   get 'pages/batman-vs-superman', to: "pages#batman_vs_superman"
+  post	'pages/save_batmen'
+  post  'pages/save_superman'
   root 'pages#batman-vs-superman' 	
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
